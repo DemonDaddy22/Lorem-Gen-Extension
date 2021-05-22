ef77f8d9925f7fe7079e261dc24db32c805a14b7
Rohan Gupta <guptarohan369@gmail.com>
2021-05-22 13:01:13 +0530

    Add website link in the footer

--- src/constants.ts
+++ src/constants.ts
@@ -10,6 +10,10 @@ export interface IFooterLink {
 export const LOREM_API_URI = 'https://demondaddy-api-den.herokuapp.com/api/v1/lorem';
 
 export const FOOTER_LINKS: IFooterLink = {
+    WEBSITE: {
+        label: 'Website',
+        uri: 'https://demondaddy22.github.io/Lorem-Gen/',
+    },
     REPO: {
         label: 'GitHub',
         uri: 'https://github.com/DemonDaddy22/Lorem-Gen-Extension/',
